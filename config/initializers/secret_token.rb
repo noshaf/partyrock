a13a72cfc5654b7e37c7172c0b83b5c5ca552920
Noah Shafi <noshaf@Noahs-MacBook-Pro.local>
2012-08-02 09:38:17 -0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partyrock::Application.config.secret_token = 'a68be9a4afabaf24aeb4f708df13632156a22e5bdf5e6c1c9082054d3849a7fc40a3ef0856bb54a3a35fc41d60311be563c9d41ceb0bc573523507a341973ebe'
