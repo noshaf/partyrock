2704b585bac0ad8b0cc9e7ea6af9a952b933bff2
Noah Shafi <noshaf@Noahs-MacBook-Pro.local>
2012-08-02 11:48:01 -0700

    changed stuff.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,19 @@ class SessionsController < ApplicationController
   def create
     auth_hash = request.env['omniauth.auth']
 
-    @authorization = Authorization.find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-      if @authorization
-        render :text => "Welcome back #{@authorization.user.name}! You have already signed up."
-      else
-        user = User.new :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]["email"]
-        user.authorizations.build :provider => auth_hash["provider"], :uid => auth_hash["uid"]
-        user.save
+    if session[:user_id]
+        # Means our user is signed in. Add the authorization to the user
+        User.find(session[:user_id]).add_provider(auth_hash)
 
-        render :text => "Hi #{user.name}! You've signed up."
+        render :text => "You can now login using #{auth_hash["provider"].capitalize} too!"
+      else
+        # Log him in or sign him up
+        auth = Authorization.find_or_create(auth_hash)
+
+        # Create the session
+        session[:user_id] = auth.user.id
+
+        render :text => "Welcome #{auth.user.name}!"
       end
   end
   
