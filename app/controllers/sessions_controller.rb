0e438f4ea89c5e79097ab980b23c8172096d99da
Noah Shafi <noshaf@x.194.247.173.web-pass.com>
2012-08-02 23:02:56 -0700

    back.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    auth_hash = request.env
+    auth_hash = request.env['omniauth.auth']
 
     render :text => auth_hash.inspect
   end
