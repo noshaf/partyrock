0613b8c2b9703c13dfb90469ef89e0459c47829d
Noah Shafi <noshaf@x.194.247.173.web-pass.com>
2012-08-02 23:56:14 -0700

    stuff.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    if User.find_by_uid(auth_hash["uid"])
-      redirect_to users_path, :alert => "Welcome Back."
-    else 
-      redirect_to new_user_path( :alert => "Thanks for signing up!", :uid => auth_hash["uid"])
-    end
+    # if User.find_by_uid(auth_hash["uid"])
+    #   redirect_to users_path, :alert => "Welcome Back."
+    # else 
+    #   redirect_to new_user_path( :alert => "Thanks for signing up!", :uid => auth_hash["uid"])
+    # end
 
     render :text => auth_hash["uid"]
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   def new
-    @uid = params[:uid]
   end
 
   def edit
