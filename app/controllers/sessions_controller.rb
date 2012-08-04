5cceb5e22730ad94e1d0a8a8acdd4d0baa2a534f
Noah Shafi <noshaf@Noahs-MacBook-Pro.local>
2012-08-03 17:32:51 -0700

    getting closer.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,11 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    uid = auth_hash["uid"].to_s
-    user = User.new(:uid => uid)
+    uid = auth_hash["uid"].inspect
+    user = User.new(:uid => "324523452")
     if user.save
       redirect_to user_path(user), :alert => "Welcome Back."
     else
-      user.save
       redirect_to new_user_path, :alert => "Thanks for signing up!"
     end
     # render :text => auth_hash["uid"]
