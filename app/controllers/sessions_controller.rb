57fa2a1d7abd31affc5f2be6537a7d758d7946f3
Noah Shafi <noshaf@Noahs-MacBook-Pro.local>
2012-08-04 12:56:49 -0700

    Added some stuff to sessions controller now that heroku is finally working.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    uid = auth_hash["uid"]
-    # user = User.new(:uid => "324523452")
-    # if user.save
-      redirect_to "/users?uid=#{uid}"
-    # else
-    #       redirect_to new_user_path, :alert => "Thanks for signing up!"
-    #     end
+    user = User.new(:uid => auth_hash["uid"])
+    if user.save
+      redirect_to user_path(user)
+    else
+      redirect_to new_user_path, :alert => "Thanks for signing up!", :uid => uid
+    end
     # render :text => auth_hash["uid"]
   end
   
