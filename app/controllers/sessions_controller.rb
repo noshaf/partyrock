fe8a202150d76a583f2291e10f64aa0b853da8c2
Noah Shafi <noshaf@Noahs-MacBook-Pro.local>
2012-08-03 17:03:38 -0700

    friggen omniauth.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-
+    raise auth_hash["uid"].class
     render :text => auth_hash["uid"]
   end
   
