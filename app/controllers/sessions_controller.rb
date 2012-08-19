82e6e0f0ff218409a40449d887d1aae87ab1d4a3
Noah Shafi <noshaf@gmail.com>
2012-08-18 21:36:19 -0700

    still fixing sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       redirect_to new_user_path, :alert => "Thanks for signing up!"
     else
       session[:user_id] = User.find_by_uid(uid).id
-      redirect_to users_path, :alert => "Welcome Back"
+      redirect_to parties_path, :alert => "Welcome Back"
     end
   end
 
