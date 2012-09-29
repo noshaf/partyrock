da5cbbd410f2d64fee123b8b202d455bd76da4a7
Noah Shafi <noshaf@gmail.com>
2012-09-29 15:18:02 -0700

    Added votes ajax

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,31 +4,33 @@ include Spotify
 	def new
 		@party = Party.find(params[:party_id])
 		@song = @party.songs.new(:name => params[:name],
-														 :artist => params[:artist],
-														 :track_key => params[:track_key])
+								 :artist => params[:artist],
+								 :track_key => params[:track_key])
 		@song.party = @party
 		@party_tracks = @party.party_tracks
 		if @song.save
 		respond_to do |format|
-      format.js
-    end
-	  else
-	  	redirect_to party_path(@party), :alert => "could not add song"
-	  end
+      		format.js
+    	end
+	  	else
+	  		redirect_to party_path(@party), :alert => "could not add song"
+	  	end
 	end
 
-  def create
-    @songs = call(params[:song][:name])[0..25]
-    @party_id = params[:song][:party_id]
-    respond_to do |format|
-      format.js
-    end
-  end
+  	def create
+    	@songs = call(params[:song][:name])[0..25]
+    	@party_id = params[:song][:party_id]
+    	respond_to do |format|
+     		format.js
+    	end
+  	end
 
-  def vote
-  	value = params[:type] == "up" ? 1 : -1
-	  @song = Song.find(params[:id])
-	  @song.add_or_update_evaluation(:votes, value, current_user)
-	  redirect_to :back
-  end
+  	def vote
+  		value = params[:type] == "up" ? 1 : -1
+		@song = Song.find(params[:id])
+	  	@song.add_or_update_evaluation(:votes, value, current_user)
+	  	respond_to do |format|
+     		format.js
+    	end
+  	end
 end
